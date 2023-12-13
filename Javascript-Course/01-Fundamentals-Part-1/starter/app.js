8011ef565e833660d0ee1b53a6ccbac752aab06d
Dre <montenegrodiussantos1@gmail.com>
2023-12-13 10:28:06 +0800

    JsFUN part 1 - assignment 1 (DONE)

--- Javascript-Course/01-Fundamentals-Part-1/starter/app.js
+++ Javascript-Course/01-Fundamentals-Part-1/starter/app.js
@@ -1,7 +1,17 @@
 let country = "Philippines";
 let continent = "Asia";
-let population = 117337368;
+let population = 155600000;
 
+// Assignment 1: Values and Variables.
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//Assignment 2: Data Types.
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
