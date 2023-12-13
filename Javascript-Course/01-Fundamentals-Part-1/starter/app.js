58ee450db9f9a70f28a28c63244b428cab974d83
Dre <montenegrodiussantos1@gmail.com>
2023-12-13 14:39:23 +0800

    JsFUN Part 1 - Assignment 4 (DONE)

--- Javascript-Course/01-Fundamentals-Part-1/starter/app.js
+++ Javascript-Course/01-Fundamentals-Part-1/starter/app.js
@@ -20,3 +20,35 @@ console.log(typeof null);
 // Assignment 3: let, const and var.
 language = "Filipino";
 console.log(language);
+
+// Assignment 4: Basic Operators.
+const populationFinland = 6000000;
+console.log(population > populationFinland);
+
+const populationAverage = 33000000;
+console.log(population > populationAverage);
+
+let description = `${country} is in ${continent}, and its ${population} people speaks ${language}`;
+console.log(description);
+
+population /= 2;
+console.log(population);
+
+population++;
+console.log(population);
+
+// <----TESTS---->
+let yearToday = 2023;
+const ageDius = yearToday - 2000;
+const ageRea = yearToday - 1999;
+console.log(ageDius, ageRea);
+
+const firstName = "Dius";
+const lastName = "Montenegro";
+console.log(`${firstName} ${lastName}`);
+
+let x = 10 + 5;
+x += 10;
+console.log(x);
+
+console.log(ageRea <= 24);
