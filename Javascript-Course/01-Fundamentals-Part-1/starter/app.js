fc8a9cfa9c4783f2dec38e2b34db408f244f2136
Dre <montenegrodiussantos1@gmail.com>
2023-12-14 11:41:21 +0800

    JsFUN Part 1 - Assignment 7 (DONE)

--- Javascript-Course/01-Fundamentals-Part-1/starter/app.js
+++ Javascript-Course/01-Fundamentals-Part-1/starter/app.js
@@ -1,46 +1,16 @@
-let country = "Philippines";
-let continent = "Asia";
-let population = 155600000;
-
-// Assignment 1: Values and Variables.
-console.log(country);
-console.log(continent);
-console.log(population);
-
-// Assignment 2: Data Types.
-let isIsland = true;
-let language;
-
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
-console.log(typeof null);
-
-// Assignment 3: let, const and var.
-language = "Filipino";
-console.log(language);
-
-// Assignment 4: Basic Operators.
-const populationFinland = 6000000;
-console.log(population > populationFinland);
-
-const populationAverage = 33000000;
-console.log(population > populationAverage);
-
-// Assignment 5: Strings and Template Literals.
-let description = `${country} is in ${continent}, and its ${population} people speaks ${language}`;
-console.log(description);
-
-// Assignment 6: Taking Decisions: if/else Statements.
-if (population > populationAverage) {
-  console.log(`${country}' population is above average 🚀`);
-} else {
-  console.log(`${country}' population is below average.`);
-}
-
-// <----TESTS---->
+// Assignment 7: Type Conversion and Coercion:
+console.log("9" - "5"); // 4
+console.log("19" - "13" + "17"); // "617"
+console.log("19" - "13" + 17); // 23
+console.log("123" < 57); // False
+console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
 
 // <----CODING CHALLENGE---->
 
 // CHALLENGE 2 - Use the BMI example from Challenge #1, and the code you already wrote, and improve it. Hint: Use an if/else statement.
+
+// <----TESTS---->
+const birthYear = "2000";
+const age = 23;
+
+console.log(Number(birthYear) + 23, typeof NaN);
