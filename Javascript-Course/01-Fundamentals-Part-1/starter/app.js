7a8af094f75d06622c6d4c22f320459ff33e9fb6
Dre <montenegrodiussantos1@gmail.com>
2023-12-14 13:11:26 +0800

    JsFUN Part 1 - Redefine Assisgnments.

--- Javascript-Course/01-Fundamentals-Part-1/starter/app.js
+++ Javascript-Course/01-Fundamentals-Part-1/starter/app.js
@@ -1,15 +1,66 @@
-// Assignment 8: Equality Operators == vs. ===.
-const numNeighbors = Number(
-  prompt("How many neighbor countires does your country have?")
-);
+// <----ASSIGNMENTS---->
 
-if (numNeighbors === 1) {
-  alert("Only 1 border!");
-} else if (numNeighbors > 1) {
-  alert("More than one border.");
-} else {
-  alert("No Border.");
-}
+const country = "Philippines";
+const continent = "Asia";
+const populationAverage = 33000000;
+const language = "Filipino";
+
+let population = 155600000;
+let populationFinland = 6000000;
+let isIsland = true;
+
+// // Assignment 1: Values and Variables.
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// // Assignment 2: Data Types.
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+// console.log(typeof null);
+
+// // Assignment 3: let, const and var.
+// console.log(language);
+
+// // Assignment 4: Basic Operators.
+// console.log(population > populationFinland);
+
+// console.log(population > populationAverage);
+
+// // Assignment 5: Strings and Template Literals.
+// const description = `${country} is in ${continent}, and its ${population} people speaks ${language}`;
+// console.log(description);
+// <----CODING CHALLENGE---->
+
+// // Assignment 6: Taking Decisions: if/else Statements.
+// if (population > populationAverage) {
+//   console.log(`${country}' population is above average 🚀`);
+// } else {
+//   console.log(`${country}' population is below average.`);
+// }
+
+// // Assignment 7: Type Conversion and Coercion.
+// console.log("9" - "5"); // 4
+// console.log("19" - "13" + "17"); // "617"
+// console.log("19" - "13" + 17); // 23
+// console.log("123" < 57); // False
+// console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
+
+// // Assignment 8: Equality Operators == vs. ===.
+// const numNeighbors = Number(
+//   prompt("How many neighbor countires does your country have?")
+// );
+
+// if (numNeighbors === 1) {
+//   alert("Only 1 border!");
+// } else if (numNeighbors > 1) {
+//   alert("More than one border.");
+// } else {
+//   alert("No Border.");
+// }
 
 // <----CODING CHALLENGE---->
 
