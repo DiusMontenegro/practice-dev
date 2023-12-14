5f6836eca6acd84410c1678b7aa3c3c0cd03436a
Dre <montenegrodiussantos1@gmail.com>
2023-12-14 16:03:16 +0800

    JsFUN Part 1 - Coding Challenge #3 (DONE)

--- Javascript-Course/01-Fundamentals-Part-1/starter/app.js
+++ Javascript-Course/01-Fundamentals-Part-1/starter/app.js
@@ -62,17 +62,57 @@ let isIsland = true;
 //   alert("No Border.");
 // }
 
-// Assignment 9: Logical Operators.
-const idealLanguage = "English";
+// // Assignment 9: Logical Operators.
+// const idealLanguage = "English";
 
-if (idealLanguage === language && population < 50000000 && !isIsland) {
-  console.log(`You should live in the ${country}! :)`);
-} else {
-  console.log(`${country} does not meet your criteria. :(`);
-}
+// if (idealLanguage === language && population < 50000000 && !isIsland) {
+//   console.log(`You should live in the ${country}! :)`);
+// } else {
+//   console.log(`${country} does not meet your criteria. :(`);
+// }
 
 // <----CODING CHALLENGE---->
 
-// CHALLENGE 2 - Use the BMI example from Challenge #1, and the code you already wrote, and improve it. Hint: Use an if/else statement.
+// CHALLENGE 3 - There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy.
 
+const minimumScore = 100;
+let teamDolphins = (96 + 108 + 89) / 2;
+let teamKoalas = (88 + 91 + 110) / 2;
+let dataSet = 1;
+
+function Result() {
+  if (teamDolphins > teamKoalas && teamDolphins >= minimumScore) {
+    console.log(`Data Set ${dataSet} result: Team Dolphins Wins!`);
+  } else if (teamDolphins < teamKoalas && teamKoalas >= minimumScore) {
+    console.log(`Data Set ${dataSet} result: Team Koalas Wins!`);
+  } else if (
+    teamDolphins === teamKoalas &&
+    teamDolphins >= minimumScore &&
+    teamKoalas >= minimumScore
+  ) {
+    console.log(`Data Set ${dataSet} result: Draw!`);
+  } else {
+    console.log(`Data Set ${dataSet} result: No one wins the competition. :(`);
+  }
+}
+
+Result();
+
+dataSet = 2;
+teamDolphins = (97 + 112 + 101) / 2;
+teamKoalas = (109 + 95 + 123) / 2;
+
+Result();
+
+dataSet = 3;
+teamDolphins = (97 + 112 + 101) / 2;
+teamKoalas = (109 + 95 + 106) / 2;
+
+Result();
+
+dataSet = 4;
+teamDolphins = (8 + 9 + 9) / 2;
+teamKoalas = (93 + 9 + 9) / 2;
+
+Result();
 // <----TESTS---->
