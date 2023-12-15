9a6dcc5ec069ffe89e41722128ad829d9f5559c4
Dre <montenegrodiussantos1@gmail.com>
2023-12-15 11:09:39 +0800

    JsFUN Part 1 - Coding Challenge 4 (DONE)

--- Javascript-Course/01-Fundamentals-Part-1/starter/app.js
+++ Javascript-Course/01-Fundamentals-Part-1/starter/app.js
@@ -15,7 +15,6 @@
 // console.log(population);
 
 // // Assignment 2: Data Types.
-
 // console.log(typeof isIsland);
 // console.log(typeof population);
 // console.log(typeof country);
@@ -27,13 +26,11 @@
 
 // // Assignment 4: Basic Operators.
 // console.log(population > populationFinland);
-
 // console.log(population > populationAverage);
 
 // // Assignment 5: Strings and Template Literals.
 // const description = `${country} is in ${continent}, and its ${population} people speaks ${language}`;
 // console.log(description);
-// <----CODING CHALLENGE---->
 
 // // Assignment 6: Taking Decisions: if/else Statements.
 // if (population > populationAverage) {
@@ -71,110 +68,50 @@
 //   console.log(`${country} does not meet your criteria. :(`);
 // }
 
-// Assignment 10: The switch Statement.
-const givenLanguage = "Filipino";
+// // Assignment 10: The switch Statement.
+// const givenLanguage = "Filipino";
 
-switch (givenLanguage) {
-  case "Mandarin":
-    console.log("MOST number of native speakers!");
-    break;
-  case "Spanish":
-    console.log("2nd place in number of native speakers");
-    break;
-  case "English":
-    console.log("3rd place in number of native speakers");
-    break;
-  case "Hindi":
-    console.log("4rth place in number of native speakers");
-    break;
-  case "Arabic":
-    console.log("5th place in number of native speakers");
-    break;
-  default:
-    console.log("Great language too :D");
-}
+// switch (givenLanguage) {
+//   case "Mandarin":
+//     console.log("MOST number of native speakers!");
+//     break;
+//   case "Spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "English":
+//     console.log("3rd place in number of native speakers");
+//     break;
+//   case "Hindi":
+//     console.log("4rth place in number of native speakers");
+//     break;
+//   case "Arabic":
+//     console.log("5th place in number of native speakers");
+//     break;
+//   default:
+//     console.log("Great language too :D");
+// }
 
 // <----CODING CHALLENGE---->
 
-// CHALLENGE 3 - There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy.
+// CHALLENGE 4 - Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 
-// const minimumScore = 100;
-// let teamDolphins = (96 + 108 + 89) / 3;
-// let teamKoalas = (88 + 91 + 110) / 3;
-// let dataSet = 1;
+const bill = Number(prompt("Please enter your bill."));
+let tip;
 
-// function Result() {
-//   if (teamDolphins > teamKoalas && teamDolphins >= minimumScore) {
-//     console.log(`Data Set ${dataSet} result: Team Dolphins Wins!`);
-//   } else if (teamDolphins < teamKoalas && teamKoalas >= minimumScore) {
-//     console.log(`Data Set ${dataSet} result: Team Koalas Wins!`);
-//   } else if (
-//     teamDolphins === teamKoalas &&
-//     teamDolphins >= minimumScore &&
-//     teamKoalas >= minimumScore
-//   ) {
-//     console.log(`Data Set ${dataSet} result: Draw!`);
-//   } else {
-//     console.log(`Data Set ${dataSet} result: No one wins the competition. :(`);
-//   }
-// }
+function Tip() {
+  if (bill >= 50 && bill <= 300) {
+    tip = bill * 0.15;
+    alert(`The bill was ${bill}, the tip was ${tip}, and the total value 
+    ${bill + tip}`);
+  } else if (bill > 0) {
+    tip = bill * 0.2;
+    alert(`The bill was ${bill}, the tip was ${tip}, and the total value 
+    ${bill + tip}`);
+  } else {
+    alert("Please enter a valid amount");
+  }
+}
 
-// Result();
+Tip();
 
-// dataSet = 2;
-// teamDolphins = (97 + 112 + 101) / 3;
-// teamKoalas = (109 + 95 + 123) / 3;
-
-// Result();
-
-// dataSet = 3;
-// teamDolphins = (97 + 112 + 101) / 3;
-// teamKoalas = (109 + 95 + 106) / 3;
-
-// Result();
-
-// dataSet = 4;
-// teamDolphins = (8 + 9 + 9) / 3;
-// teamKoalas = (93 + 9 + 9) / 3;
-
-// Result();
 // <----TESTS---->
-let day = "thursday";
-
-switch (day) {
-  case "monday":
-    console.log("Plan");
-    console.log("code");
-    break;
-  case "tuesday":
-    console.log("Prepare code");
-    break;
-  case "wednesday":
-  case "thursday":
-    console.log("Implement code");
-    break;
-  case "friday":
-    console.log("reflect code");
-    break;
-  case "saturday":
-  case "sunday":
-    console.log("code and enjoy the weekend");
-    break;
-  default:
-    console.log("Enter a valid day.");
-}
-
-if (day === "monday") {
-  console.log("Plan");
-  console.log("code");
-} else if (day === "tuesday") {
-  console.log("Prepare code");
-} else if (day === "wednesday" || day === "thursday") {
-  console.log("Implement code");
-} else if (day === "friday") {
-  console.log("reflect code");
-} else if (day === "saturday" || day === "sunday") {
-  console.log("code and enjoy the weekend");
-} else {
-  console.log("Enter a valid day.");
-}
