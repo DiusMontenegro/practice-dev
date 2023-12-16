8d85fed056d232a64c356cf77b577737649f7ca0
Dre <montenegrodiussantos1@gmail.com>
2023-12-16 18:33:35 +0800

    JsFUN Part 2 - Coding Challenge 1 Simpler Version (DONE)

--- Javascript-Course/02-Fundamentals-Part-2/starter/app.js
+++ Javascript-Course/02-Fundamentals-Part-2/starter/app.js
@@ -1,15 +1,12 @@
 "use strict";
 
-// <----CODING CHALLENGES---->
+// // <----CODING CHALLENGES---->
 
 // Coding Challenge 1 - Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently. Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
 
-const calcAverage = (score1, score2, score3) => {
-  let average = (score1 + score2 + score3) / 3;
-  return average;
-};
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-function checkWinner(avgDolphins, avgKoalas) {
+const checkWinner = (avgDolphins, avgKoalas) => {
   if (avgDolphins >= avgKoalas * 2) {
     console.log(`Team Dolphins win (${avgDolphins} vs ${avgKoalas})`);
   } else if (avgKoalas >= avgDolphins * 2) {
@@ -17,10 +14,12 @@ function checkWinner(avgDolphins, avgKoalas) {
   } else {
     console.log("No team wins");
   }
-}
+};
 
-let avgDolphins = calcAverage(44, 23, 71);
-let avgKoalas = calcAverage(65, 54, 49);
+let avgDolphins, avgKoalas;
+
+avgDolphins = calcAverage(44, 23, 71);
+avgKoalas = calcAverage(65, 54, 49);
 checkWinner(avgDolphins, avgKoalas);
 
 avgDolphins = calcAverage(85, 54, 41);
