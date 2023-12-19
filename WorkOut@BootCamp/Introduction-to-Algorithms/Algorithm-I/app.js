21872c30d527a21bcfbc811dc06428cda6e98c0e
Dre <montenegrodiussantos1@gmail.com>
2023-12-20 07:07:23 +0800

    VsCode UI test commit

--- WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-I/app.js
+++ WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-I/app.js
@@ -1,1 +1,10 @@
-console.log("Hello V88");
+const v88EngineStart = function () {
+  for (let i = 10; i >= 0; i--) {
+    console.log(`Welcome to V88! The training starts in ${i}`);
+    if (i === 0) {
+      console.log("Let's Begin!");
+    }
+  }
+};
+
+v88EngineStart();
