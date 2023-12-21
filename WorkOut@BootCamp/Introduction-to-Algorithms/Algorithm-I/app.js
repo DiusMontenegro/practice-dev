6c1a23c7348c73b6f4de5f088774213ed057eb62
Dre <montenegrodiussantos1@gmail.com>
2023-12-21 20:19:34 +0800

    T-diagram test V1.0

--- WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-I/app.js
+++ WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-I/app.js
@@ -1,10 +1,43 @@
-const v88EngineStart = function () {
-  for (let i = 10; i >= 0; i--) {
-    console.log(`Welcome to V88! The training starts in ${i}`);
-    if (i === 0) {
-      console.log("Let's Begin!");
-    }
+// const v88EngineStart = function () {
+//   for (let i = 10; i >= 0; i--) {
+//     console.log(`Welcome to V88! The training starts in ${i}`);
+//     if (i === 0) {
+//       console.log("Let's Begin!");
+//     }
+//   }
+// };
+
+// v88EngineStart();
+
+let a = 35;
+hello(a);
+console.log(a);
+function hello(a) {
+  a = 50;
+  console.log(a);
+  return a;
+}
+
+// a = hello(a);
+
+// console.log(a);
+
+const arrOne = [1, 2, 3, 4, 5];
+const arrTwo = [10, 20, 30, 40, 50];
+const arrThree = [100, 200, 300, 400, 500];
+
+const arrayPrint = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
   }
 };
 
-v88EngineStart();
+arrayPrint(arrOne);
+arrayPrint(arrTwo);
+arrayPrint(arrThree);
+
+function namePrint(yourName) {
+  return `Hello, ${yourName}`;
+}
+
+console.log(namePrint("Dre"));
