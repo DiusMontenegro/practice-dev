fad2b15fb667a70b161ebae0cb66511a0b59f6da
Dre <montenegrodiussantos1@gmail.com>
2023-12-24 17:54:59 +0800

    Developer-Skills Part 03 - Problem Upload

--- Javascript-Course/03-Developer-Skills/starter/script.js
+++ Javascript-Course/03-Developer-Skills/starter/script.js
@@ -11,3 +11,14 @@ const calcAge = birthYear => 2023 - birthYear;
 console.log(x);
 
 // BUG FIXME
+
+console.log(calcAge(2000));
+
+// PROBLEM
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temparature of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temparatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understand the problem
+
+// 2) Breaking up into sub problems
