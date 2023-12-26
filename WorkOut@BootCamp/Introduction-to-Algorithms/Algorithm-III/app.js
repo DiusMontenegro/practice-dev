b36dedd955aa9b5a9248de04d5cd1b9e73d80580
Dre <montenegrodiussantos1@gmail.com>
2023-12-26 15:59:06 +0800

    Algorithm 3 - (COMPLETED)

--- WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-III/app.js
+++ WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-III/app.js
@@ -2,45 +2,45 @@
 
 // Q1 : (Print 1 to x). Please complete the codes below to have the function print all the integers from 1 to x. If x is negative, have it print/log "negative number" and have the function return false.
 
-// function printUpTo(x) {
-//   if (x < 0) {
-//     console.log("negative number");
-//     return false;
-//   } else {
-//     for (let i = 1; i <= x; i++) {
-//       console.log(i);
-//     }
-//   }
-// }
+function printUpTo(x) {
+  if (x < 0) {
+    console.log("negative number");
+    return false;
+  } else {
+    for (let i = 1; i <= x; i++) {
+      console.log(i);
+    }
+  }
+}
 
-// printUpTo(1000);
-// y = printUpTo(-1);
-// console.log(y);
+printUpTo(1000);
+y = printUpTo(-1);
+console.log(y);
 
 // Q2 : "PrintSum" . Please complete the code below to have the function print integers from 0 to x and with each integer print the sum so far.  Have the function return the final sum
 
-// function PrintSum(x) {
-//   let sum = 0;
-//   for (let i = 0; i <= x; i++) {
-//     sum += i;
-//   }
-//   return sum;
-// }
+function PrintSum(x) {
+  let sum = 0;
+  for (let i = 0; i <= x; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
-// y = PrintSum(255);
-// console.log(y);
+y = PrintSum(255);
+console.log(y);
 
 // Q3 : "PrintSumArray" . Please complete the code below to have the function return the sum of all the values in a given array.
 
-// function PrintSumArray(x) {
-//   let sum = 0;
-//   for (let i = 0; i < x.length; i++) {
-//     sum += x[i];
-//   }
-//   return sum;
-// }
+function PrintSumArray(x) {
+  let sum = 0;
+  for (let i = 0; i < x.length; i++) {
+    sum += x[i];
+  }
+  return sum;
+}
 
-// console.log(PrintSumArray([1, 2, 3]));
+console.log(PrintSumArray([1, 2, 3]));
 
 // Q4 : (BONUS) "LargestElement" . Create a function that returns the largest element in a given array.  For example largestElement( [1,30,5,7] ) should return 30.
 
