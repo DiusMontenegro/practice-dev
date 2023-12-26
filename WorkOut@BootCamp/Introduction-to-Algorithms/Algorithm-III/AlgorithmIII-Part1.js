112eb30b79e96521e55542cdcb1f63d49bf3344c
Dre <montenegrodiussantos1@gmail.com>
2023-12-26 14:36:34 +0800

    Algorithm 3 - Part 1, Q2 (ANSWERED)

--- WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-III/AlgorithmIII-Part1.js
+++ WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-III/AlgorithmIII-Part1.js
@@ -1,6 +1,6 @@
 // Part 1 - Written Assignment (Predict the output.)
 
-// 1.
+// 1. Output: 2, 3, undefined
 console.log("----- PART 1 -----");
 
 function multiply(x, y) {
@@ -10,3 +10,11 @@ function multiply(x, y) {
 
 b = multiply(2, 3);
 console.log(b);
+
+// 2. Output: 6, 10
+function multiply1(x, y) {
+  return x * y;
+}
+b = multiply1(2, 3);
+console.log(b);
+console.log(multiply1(5, 2));
