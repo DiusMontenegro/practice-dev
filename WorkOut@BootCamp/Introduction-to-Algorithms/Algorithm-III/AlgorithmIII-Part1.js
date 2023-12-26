2157fa84ad51ec17e2c091be4ae2f8dae671f230
Dre <montenegrodiussantos1@gmail.com>
2023-12-26 14:37:27 +0800

    Algorithm 3 - Part 1, Q3 (ANSWERED)

--- WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-III/AlgorithmIII-Part1.js
+++ WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-III/AlgorithmIII-Part1.js
@@ -18,3 +18,10 @@ function multiply1(x, y) {
 b = multiply1(2, 3);
 console.log(b);
 console.log(multiply1(5, 2));
+
+// 3. Output: 3, 7
+let x = [1, 2, 3, 4, 5, 10];
+for (let i = 0; i < 5; i++) {
+  i += 3;
+  console.log(i);
+}
