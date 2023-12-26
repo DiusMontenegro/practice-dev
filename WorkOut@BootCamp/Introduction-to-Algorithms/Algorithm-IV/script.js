1383f73acec60ca0c211eb127974a1e32b054d16
Dre <montenegrodiussantos1@gmail.com>
2023-12-27 05:02:08 +0800

    Algorithm IV - Part 1 Assignment. Q2 (DONE)

--- WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-IV/script.js
+++ WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-IV/script.js
@@ -9,3 +9,17 @@ function a(x, y) {
 }
 
 console.log(a(5, 5));
+
+// 2. Output: [ 2, 2, 5], [ 2, 2, 5], [ 6, 8, 5], [ 6, 8, 5]
+function a2(x, y) {
+  const z = [];
+  z.push(x);
+  z.push(y);
+  z.push(5);
+  console.log(z);
+  return z;
+}
+
+let b = a2(2, 2);
+console.log(b);
+console.log(a2(6, 8));
