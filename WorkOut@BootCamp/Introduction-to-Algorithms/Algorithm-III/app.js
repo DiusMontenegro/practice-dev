240b31b4629daee3de103d497f59882250eef4fb
Dre <montenegrodiussantos1@gmail.com>
2023-12-26 15:14:26 +0800

    Algorithm 3 - Part 2, Q1 (ANSWERED)

--- WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-III/app.js
+++ WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-III/app.js
@@ -1,1 +1,18 @@
+// Part 2: For these, without using any built-in functions (but only using the ones that you're already taught (e.g. push, pop), complete each challenge below.  Put all of these in a single file so that it's easier to review.
 
+// Q1 : (Print 1 to x). Please complete the codes below to have the function print all the integers from 1 to x. If x is negative, have it print/log "negative number" and have the function return false.
+
+function printUpTo(x) {
+  if (x < 0) {
+    console.log("negative number");
+    return false;
+  } else {
+    for (let i = 1; i <= x; i++) {
+      console.log(i);
+    }
+  }
+}
+
+printUpTo(1000);
+y = printUpTo(-1);
+console.log(y);
