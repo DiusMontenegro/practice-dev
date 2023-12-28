88b0573f6de86db004f180a32e960f94013b9c0d
Dre <montenegrodiussantos1@gmail.com>
2023-12-28 09:46:30 +0800

    Algorithm V - Assignment Q2 (DONE)

--- WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-V/app.js
+++ WorkOut@BootCamp/Introduction-to-Algorithms/Algorithm-V/app.js
@@ -17,3 +17,46 @@ const greaterThanY = function (x) {
 let y = 5;
 const numArr = [6, 2, 3, 8, y, 7, 9];
 greaterThanY(numArr);
+
+// 2. Given an array, print the max, min, and average values for that array.
+const calcMax = function (x) {
+  let max = x[0];
+  for (let i = 0; i < x.length; i++) {
+    if (max < x[i]) {
+      max = x[i];
+    }
+  }
+  return max;
+};
+
+const calcMin = function (x) {
+  let min = x[0];
+  for (let i = 0; i < x.length; i++) {
+    if (min > x[i]) {
+      min = x[i];
+    }
+  }
+  return min;
+};
+
+const calcAverage = function (x) {
+  let sum = 0;
+  for (let i = 0; i < x.length; i++) {
+    sum += x[i];
+  }
+  const average = sum / x.length;
+  return average;
+};
+
+const printMaxMinAverage = function (x) {
+  const min = calcMin(x);
+  const max = calcMax(x);
+  const average = calcAverage(x);
+  console.log(min);
+  console.log(max);
+  console.log(average);
+};
+
+//Example:
+y = [-1, 5, 3];
+printMaxMinAverage(y);
