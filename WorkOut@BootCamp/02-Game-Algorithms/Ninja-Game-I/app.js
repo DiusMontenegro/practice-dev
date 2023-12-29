b506ac32653dc9342107f3ae64acf8627ec3f031
Dre <montenegrodiussantos1@gmail.com>
2023-12-29 17:22:43 +0800

    Add Ninja Movements

--- WorkOut@BootCamp/02-Game-Algorithms/Ninja-Game-I/app.js
+++ WorkOut@BootCamp/02-Game-Algorithms/Ninja-Game-I/app.js
@@ -1,1 +1,25 @@
 "use strict";
+
+let topValue = 100,
+  leftValue = 450;
+
+function update() {
+  document.getElementById("character").style.top = topValue + "px";
+  document.getElementById("character").style.left = leftValue + "px";
+}
+
+document.onkeydown = function (e) {
+  if (e.key === "ArrowLeft") {
+    leftValue -= 10;
+  }
+  if (e.key === "ArrowRight") {
+    leftValue += 10;
+  }
+  if (e.key === "ArrowUp") {
+    topValue -= 10;
+  }
+  if (e.key === "ArrowDown") {
+    topValue += 10;
+  }
+  update();
+};
