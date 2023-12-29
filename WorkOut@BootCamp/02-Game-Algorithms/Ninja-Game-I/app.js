a39b94cac7b8e35f6dd2621e829ae93824af39c7
Dre <montenegrodiussantos1@gmail.com>
2023-12-29 20:48:03 +0800

    Add Dynamic Ninja Movement

--- WorkOut@BootCamp/02-Game-Algorithms/Ninja-Game-I/app.js
+++ WorkOut@BootCamp/02-Game-Algorithms/Ninja-Game-I/app.js
@@ -1,22 +1,37 @@
 "use strict";
 
 let topValue = 100,
-  leftValue = 450;
+  leftValue = 450,
+  walkValue = 1,
+  direction = "down";
 
 function update() {
   document.getElementById("character").style.top = topValue + "px";
   document.getElementById("character").style.left = leftValue + "px";
+  document.getElementById(
+    "character"
+  ).style.backgroundImage = `url("./img/${direction}${walkValue}.png")`;
 }
 
 document.onkeydown = function (e) {
-  if (e.key === "ArrowLeft" && leftValue > 0) {
+  if (walkValue === 1) {
+    walkValue = 2;
+  } else if (walkValue === 2) {
+    walkValue = 1;
+  }
+
+  if (e.key === "ArrowLeft") {
     leftValue -= 10;
-  } else if (e.key === "ArrowRight" && leftValue <= 500) {
+    direction = "left";
+  } else if (e.key === "ArrowRight") {
     leftValue += 10;
-  } else if (e.key === "ArrowUp" && topValue > 0) {
+    direction = "right";
+  } else if (e.key === "ArrowUp") {
     topValue -= 10;
-  } else if (e.key === "ArrowDown" && topValue <= 500) {
+    direction = "top";
+  } else if (e.key === "ArrowDown") {
     topValue += 10;
+    direction = "down";
   }
   update();
 };
