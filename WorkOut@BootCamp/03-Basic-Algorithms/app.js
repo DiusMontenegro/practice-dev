c767ed9992f60a23b0e2d10971f8c9d4bbe43fa2
Dre <montenegrodiussantos1@gmail.com>
2023-12-30 21:09:22 +0800

    Basic Algorithms - Challenge 6 (DONE)

--- WorkOut@BootCamp/03-Basic-Algorithms/app.js
+++ WorkOut@BootCamp/03-Basic-Algorithms/app.js
@@ -67,3 +67,18 @@ console.log(returnArrayCountGreaterThanY([2, 3, 5], 4));
 console.log(returnArrayCountGreaterThanY([2, 3, 5], 1));
 console.log(returnArrayCountGreaterThanY([4, 10, 15], 10));
 console.log(returnArrayCountGreaterThanY([4, 10, 15], 20));
+
+// 6. Implement function sigma(num) that given a number, returns the sum of all positive integers up to a number (inclusive). Ex.:sigma(3)=6(or1 + 2 + 3); sigma(5)=15(or1 + 2 + 3 + 4 + 5).
+
+function sigma(num) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(sigma(3));
+console.log(sigma(5));
+console.log(sigma(6));
+console.log(sigma(8));
