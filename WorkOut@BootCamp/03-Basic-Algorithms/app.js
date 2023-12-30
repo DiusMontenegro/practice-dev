bbb50430150b54b57d0e33346ebeb2acd2d45d0f
Dre <montenegrodiussantos1@gmail.com>
2023-12-30 17:50:21 +0800

    Basic Algortihm - Challenge 1 (DONE)

--- WorkOut@BootCamp/03-Basic-Algorithms/app.js
+++ WorkOut@BootCamp/03-Basic-Algorithms/app.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// 1. Create a function sum() that returns the sum of the two numbers passed as its arguments. For example, sum(2,5) should return 7; sum(3,10) should return 13.
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(3, 10));
