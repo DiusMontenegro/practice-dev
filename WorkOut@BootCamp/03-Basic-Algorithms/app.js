6f4b8aaf43c7c5f021a7cc3b9d925c60987aab18
Dre <montenegrodiussantos1@gmail.com>
2023-12-30 21:53:12 +0800

    Basic Algorithms - Challenge 9 (DONE)

--- WorkOut@BootCamp/03-Basic-Algorithms/app.js
+++ WorkOut@BootCamp/03-Basic-Algorithms/app.js
@@ -109,3 +109,20 @@ function swapTowardCenter(arr) {
 console.log(swapTowardCenter([true, 42, "Ada", 2, "pizza"]));
 console.log(swapTowardCenter([1, 2, 3, 4, 5, 6]));
 console.log(swapTowardCenter(["hi", "coding", "dojo", "fans"]));
+
+// 9. Create threesFives(n) that adds values from 1 and n (inclusive) if that value is not divisible by 3 or 5. Return the final sum. For example, threesFives(10) returns 22 as it only returns the sum of 1, 2, 4, 7, and 8. In that example, it skips 3, 6, and 9 as those are divisible by 3. It also skips 5, and 10 as it's divisible by 5.
+
+function threesFives(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      continue;
+    } else {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(threesFives(10));
+console.log(threesFives(15));
