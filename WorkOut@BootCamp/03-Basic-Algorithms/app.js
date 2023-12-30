99610a7d14a5908d7920e5baafb3a053394e9148
Dre <montenegrodiussantos1@gmail.com>
2023-12-30 20:44:04 +0800

    Basic Algorithms - Challenge 2 (DONE)

--- WorkOut@BootCamp/03-Basic-Algorithms/app.js
+++ WorkOut@BootCamp/03-Basic-Algorithms/app.js
@@ -7,3 +7,14 @@ function sum(num1, num2) {
 }
 
 console.log(sum(3, 10));
+
+// 2. Kelvin wants to convert between temperature scales. Create celciusToFahrenheit(cDegrees) that accepts a number of degrees in Celcius, and returns the equivalent temperature as expressed in Fahrenheit degrees. For review, Fahrenheit = (9/5 * Celsius) + 32.
+
+function celciusToFahrenheit(cDegrees) {
+  let fahrenheit = (cDegrees * 9) / 5 + 32;
+  return fahrenheit;
+}
+
+console.log(celciusToFahrenheit(0));
+console.log(celciusToFahrenheit(10));
+console.log(celciusToFahrenheit(100));
