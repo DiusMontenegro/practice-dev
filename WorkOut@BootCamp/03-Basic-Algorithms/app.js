9a4c257ba9ed56d6678cf114d574fe907ca16d20
Dre <montenegrodiussantos1@gmail.com>
2023-12-31 11:27:25 +0800

    Basic Algorithms - Challenge 13 (DONE)

--- WorkOut@BootCamp/03-Basic-Algorithms/app.js
+++ WorkOut@BootCamp/03-Basic-Algorithms/app.js
@@ -182,3 +182,25 @@ console.log(clockHandAngles(3600));
 console.log(clockHandAngles(10800));
 console.log(clockHandAngles(5000));
 console.log(clockHandAngles(8000));
+
+// 13. Return whether a given integer is prime. Prime numbers are only evenly divisible by themselves and 1. Many highly optimized solutions exist, but for now, just create one that is easy to understand and debug. For example, isPrime(3) should return true. isPrime(4) should return false as 4 is divisible by 2.
+
+function isPrime(num) {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isPrime(3));
+console.log(isPrime(4));
+console.log(isPrime(13));
+console.log(isPrime(65));
+console.log(isPrime(17));
