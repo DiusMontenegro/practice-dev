c08313349e12970526a333be1027e446ada8d10c
Dre <montenegrodiussantos1@gmail.com>
2024-01-01 18:49:56 +0800

    Basic Algorithms - Challenge 14 (DONE)

--- WorkOut@BootCamp/03-Basic-Algorithms/app.js
+++ WorkOut@BootCamp/03-Basic-Algorithms/app.js
@@ -204,3 +204,25 @@ console.log(isPrime(4));
 console.log(isPrime(13));
 console.log(isPrime(65));
 console.log(isPrime(17));
+
+// 14. Create the extractDigit(num,digitNum) function that given a number and a digit number, returns the numeral value of that digit. 0 represents the ones digit, 1 represents the tens digit, etc. Given (1824,2), return 8. Given (1824,0), return 4. Given (1824,7), return 0.
+
+// Second: handle negative digitNum values, where -1 represents tenths digit (0.x), -2 represents hundredths digit (0.0x), etc. Given (1234.56,-1), return 5.
+
+function extractDigit(num, digitNum) {
+  // Handle negative digitNum values
+  if (digitNum < 0) {
+    const scale = Math.pow(10, Math.abs(digitNum));
+    return Math.floor(num * scale) % 10;
+  }
+
+  // Handle non-negative digitNum values
+  const divisor = Math.pow(10, digitNum);
+  return Math.floor(num / divisor) % 10;
+}
+
+console.log(extractDigit(199.35, 0));
+console.log(extractDigit(1824.2, 2));
+console.log(extractDigit(199.35, 0));
+console.log(extractDigit(199.35, -1));
+console.log(extractDigit(199.35, -2));
