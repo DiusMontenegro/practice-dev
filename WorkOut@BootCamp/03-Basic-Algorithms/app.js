3b8a389f01eba5ae388ff179b263a5425a8a7b9c
Dre <montenegrodiussantos1@gmail.com>
2024-01-02 23:52:01 +0800

    Basic Algorithms - Challenge 26 (DONE)

--- WorkOut@BootCamp/03-Basic-Algorithms/app.js
+++ WorkOut@BootCamp/03-Basic-Algorithms/app.js
@@ -475,3 +475,56 @@ console.log(bubbleSort([5, 3, 1, 2]));
 console.log(bubbleSort([5, 4, 3, 2, 1]));
 console.log(bubbleSort([5, 3, 1, -1, -3]));
 console.log(bubbleSort([5, 3, 1]));
+
+// 26. Create a function that prints a multiplication table such as follows:
+
+// multiTable(2,2) should print
+
+// 1 2
+
+// 2 4
+
+// multiTable(3,3) should print
+
+// 1 2 3
+
+// 2 4 6
+
+// 3 6 9
+
+// multiTable(4,4) should print
+
+// 1 2 3 4
+
+// 2 4 6 8
+
+// 3 6 9 12
+
+// 4 8 12 16
+
+// multiTable(2,4) should print
+
+// 1 2
+
+// 2 4
+
+// 3 6
+
+// 4 8
+
+// For this challenge, it could print/log all of these numbers in a single row or as many rows as you want. As long as it displays the numbers in the right sequence, it will pass the test cases.
+
+function multiTable(x, y) {
+  for (let i = 1; i <= y; i++) {
+    for (let j = 1; j <= x; j++) {
+      const product = i * j;
+      console.log(product);
+    }
+  }
+}
+
+// Test cases
+console.log(multiTable(2, 2));
+console.log(multiTable(3, 3));
+console.log(multiTable(2, 4));
+console.log(multiTable(5, 8));
