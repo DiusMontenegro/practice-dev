a2b87f77aec162c6bf96bb4709745255286f3ef9
Dre <montenegrodiussantos1@gmail.com>
2024-01-17 21:23:35 +0800

    WebFun - jQuery Mini Documentation (DONE)

--- WorkOut@BootCamp/05-Web-Fundamentals/04-jQuery/01-Mini-Documentation/script.js
+++ WorkOut@BootCamp/05-Web-Fundamentals/04-jQuery/01-Mini-Documentation/script.js
@@ -11,6 +11,43 @@ document.addEventListener("DOMContentLoaded", function() {
     $("#toggle").click(function(){
         $(".toggle").toggle();
     });
-    
-
+    $("#slideDown").click(function(){
+        $(".slideDown").slideDown();
+    });
+    $("#slideUp").click(function(){
+        $(".slideUp").slideUp();
+    });
+    $("#slideToggle").click(function(){
+        $(".slideToggle").slideToggle();
+    });
+    $("#fadeIn").click(function(){
+        $(".fadeIn").fadeIn();
+    });
+    $("#fadeOut").click(function(){
+        $(".fadeOut").fadeOut();
+    });
+    $("#addClass").click(function(){
+        $(".addClass").addClass("bg-blue");
+    });
+    $("#before").click(function(){
+        $(".before").before(`<img src="./img/shuriken.png" alt="madara" class="shuriken"></img>`);
+    });
+    $("#after").click(function(){
+        $(".after").after(`<img src="./img/bullet.svg" alt="bullet" class="bullet"></img>`);
+    });
+    $("#append").click(function(){
+        $(".append").append(`<span>HA</span>`);
+    });
+    $("#html").click(function(){
+        $(".html").html(`<a href='#html'>I am a link inside the list</a>`);
+    });
+    $("#attr").click(function(){
+        alert(`My "type" Attribute value is: ${$(this).attr("type")}`);
+    });
+    $("#val").click(function(){
+        $('.val').val("Value is now set!");
+    });
+    $("#text").click(function(){
+        $('.text').text("Send me Gcash 😂");
+    });
 });
