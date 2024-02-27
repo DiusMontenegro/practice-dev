055967e729d4746085cafd3649f169e461350ef6
Dre <montenegrodiussantos1@gmail.com>
2024-02-27 13:26:21 +0800

    JavascriptFUN (Learn to Code) - This length that value (DONE)

---
+++ WorkOut@BootCamp/07-Javascript/02-Learn-to-code/21-thisLengthThatValue.js
@@ -0,0 +1,12 @@
+function thisLengthThatValue (num1, num2) {
+
+    let newArr = [];
+
+    for (let i = 0; i < num1; i++) {
+        newArr.push(num2);
+    }
+
+    return newArr;
+}
+
+thisLengthThatValue();
