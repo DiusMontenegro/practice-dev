6f8a7c669765f476c146ed24936d727bc9b12777
Dre <montenegrodiussantos1@gmail.com>
2024-02-28 05:57:39 +0800

    JavascriptFUN (Adv. DSA) - String Anagrams (DONE)

--- WorkOut@BootCamp/07-Javascript/04-Adv-DataStructures-and-Algorithms/05-stringAnagrams.js
+++ WorkOut@BootCamp/07-Javascript/04-Adv-DataStructures-and-Algorithms/05-stringAnagrams.js
@@ -5,16 +5,45 @@ function StringAnagram(str) {
 }
 
 function _rStringAnagram(name, rem, words) {
+
+    // let nameDepth = "";
+    // for(let i = 0; i < name.length; i++) {
+    //     nameDepth += '_________'
+    // }
+
     if (rem === "") {
         words.push(name);
-    } else {
-        for (let i = 0; i < rem.length; i++) {
-            let newWord = name + rem[i];
-            let newRem = rem.slice(0, i) + rem.slice(i + 1);
-            _rStringAnagram(newWord, newRem, words);
+    }
+
+    for (let i = 0; i < rem.length; i++) {
+        let newRem = "";
+        for (let j = 0; j < rem.length; j++) {
+            if (i !== j) {
+                newRem += rem[j];
+            }
         }
+
+        _rStringAnagram(name + rem[i], newRem, words);
     }
 }
+
+// function StringAnagram(str) {
+//     let words = [];
+//     _rStringAnagram("", str, words);
+//     return words;
+// }
+
+// function _rStringAnagram(name, rem, words) {
+//     if (rem === "") {
+//         words.push(name);
+//     } else {
+//         for (let i = 0; i < rem.length; i++) {
+//             let newWord = name + rem[i];
+//             let newRem = rem.slice(0, i) + rem.slice(i + 1);
+//             _rStringAnagram(newWord, newRem, words);
+//         }
+//     }
+// }
 
 // Testing the function
 let words = StringAnagram("lim");
