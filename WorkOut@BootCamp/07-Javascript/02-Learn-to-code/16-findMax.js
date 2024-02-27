719fcbe5ced3db6f1f6119ca0eb6f3e6f2ce8f29
Dre <montenegrodiussantos1@gmail.com>
2024-02-27 12:44:47 +0800

    JavascriptFUN (Learn to Code) - Find max (DONE)

---
+++ WorkOut@BootCamp/07-Javascript/02-Learn-to-code/16-findMax.js
@@ -0,0 +1,14 @@
+function findMax(arr) {
+
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+findMax();
