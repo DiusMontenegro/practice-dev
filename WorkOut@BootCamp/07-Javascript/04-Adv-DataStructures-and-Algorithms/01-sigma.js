b8bbf3e811f421f32406a7f40ae86dfe4cc6d333
Dre <montenegrodiussantos1@gmail.com>
2024-02-28 04:22:40 +0800

    JavascriptFUN (Adv. DSA) - Sigma (DONE)

--- WorkOut@BootCamp/07-Javascript/04-Adv-DataStructures-and-Algorithms/01-sigma.js
+++ WorkOut@BootCamp/07-Javascript/04-Adv-DataStructures-and-Algorithms/01-sigma.js
@@ -1,3 +1,4 @@
+// No recursion
 function sigma(n){
 
     let sigma = 0;
@@ -9,7 +10,23 @@ function sigma(n){
     return sigma;
 }
 
+// With recursion
+function sigma2(n) {
+
+    if (n < 1) {
+        return n;
+    }
+
+    return n + sigma2(n - 1);
+}
+
+
 console.log(sigma(3));
 console.log(sigma(5));
 console.log(sigma(7));
 console.log(sigma(10));
+
+console.log(sigma2(3));
+console.log(sigma2(5));
+console.log(sigma2(7));
+console.log(sigma2(10));
