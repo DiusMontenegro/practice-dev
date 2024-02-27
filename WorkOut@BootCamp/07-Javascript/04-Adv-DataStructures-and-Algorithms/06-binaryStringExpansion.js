22153ad68a100d2eb0fd50e827ed612236c50cb2
Dre <montenegrodiussantos1@gmail.com>
2024-02-28 06:26:20 +0800

    JavascriptFUN (Adv. DSA) - Binary string expansion (DONE)

--- WorkOut@BootCamp/07-Javascript/04-Adv-DataStructures-and-Algorithms/06-binaryStringExpansion.js
+++ WorkOut@BootCamp/07-Javascript/04-Adv-DataStructures-and-Algorithms/06-binaryStringExpansion.js
@@ -1,25 +1,60 @@
 function BinaryStringExpansion(str) {
-    const result = [];
-    generateBinaryStrings(str, "", result);
-    return result;
+    words = [];
+    _rBinaryStringExpansion ("", str, words);
+    return words;
 }
 
-function generateBinaryStrings(input, current, result) {
-    if (input.length === 0) {
-        result.push(current);
+function _rBinaryStringExpansion(name, rem, words) {
+
+    // let nameDepth = "";
+    // for(let i = 0; i < name.length; i++) {
+    //     nameDepth += '___'
+    // }
+    if (rem === "") {
+        // console.log(name);
+        words.push(name);
         return;
     }
 
-    const firstChar = input[0];
-    const remainingChars = input.slice(1);
+    let newRem = "";
 
-    if (firstChar === '?') {
-        generateBinaryStrings(remainingChars, current + '0', result);
-        generateBinaryStrings(remainingChars, current + '1', result);
+    for(let i = 1; i < rem.length; i++) {
+        newRem += rem[i];
+    }
+
+    if (rem[0] === "?") {
+        // console.log(nameDepth + name + '1');
+        _rBinaryStringExpansion(name + "1", newRem, words);
+        // console.log(nameDepth + name + '0');
+        _rBinaryStringExpansion(name + "0", newRem, words);
     } else {
-        generateBinaryStrings(remainingChars, current + firstChar, result);
+        // console.log(nameDepth + name + rem[0]);
+        _rBinaryStringExpansion(name + rem[0], newRem, words);
     }
 }
+
+// function BinaryStringExpansion(str) {
+//     const result = [];
+//     generateBinaryStrings(str, "", result);
+//     return result;
+// }
+
+// function generateBinaryStrings(input, current, result) {
+//     if (input.length === 0) {
+//         result.push(current);
+//         return;
+//     }
+
+//     const firstChar = input[0];
+//     const remainingChars = input.slice(1);
+
+//     if (firstChar === '?') {
+//         generateBinaryStrings(remainingChars, current + '0', result);
+//         generateBinaryStrings(remainingChars, current + '1', result);
+//     } else {
+//         generateBinaryStrings(remainingChars, current + firstChar, result);
+//     }
+// }
 
 // Test cases
 console.log(BinaryStringExpansion("0?0??").length);    // Should return 8
