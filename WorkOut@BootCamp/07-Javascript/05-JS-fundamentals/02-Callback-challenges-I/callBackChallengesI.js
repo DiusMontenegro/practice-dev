87c6200dcfa2244b72f5404bbd737307ca88a773
Dre <montenegrodiussantos1@gmail.com>
2024-02-29 10:14:05 +0800

    JavascriptFUN (JS Fundamentals) - dQuery (DONE)

--- WorkOut@BootCamp/07-Javascript/05-JS-fundamentals/02-Callback-challenges-I/callBackChallengesI.js
+++ WorkOut@BootCamp/07-Javascript/05-JS-fundamentals/02-Callback-challenges-I/callBackChallengesI.js
@@ -1,13 +1,12 @@
 // 1.
-let result = foreach([1,2,3,4,5], function(num) {
-    return num*2;
+let result = foreach([1, 2, 3, 4, 5], function (num) {
+    return num * 2;
 });
 
 function foreach(arr_num, operation) {
-
     let newArr = [];
     for (let i = 0; i < arr_num.length; i++) {
-        newArr.push(operation(arr_num[i]));
+        newArr[i] = (operation(arr_num[i]));
     }
     return newArr;
 }
@@ -16,11 +15,10 @@ console.log(result);
 
 // 2.
 
-result = foreach([1,2,3,"v88", "training"], function(val) {
-    if(typeof(val) === 'number') {
+result = foreach([1, 2, 3, 'v88', 'training'], function (val) {
+    if (typeof val === 'number') {
         return 0;
-    }
-    else {
+    } else {
         return val;
     }
 });
@@ -28,7 +26,7 @@ result = foreach([1,2,3,"v88", "training"], function(val) {
 function foreach(arr, operation) {
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
-        newArr.push(operation(arr[i]));
+        newArr[i] = (operation(arr[i]));
     }
     return newArr;
 }
@@ -36,14 +34,14 @@ function foreach(arr, operation) {
 console.log(result); //this should log [0,0,0,"v88","training"];
 
 // 3.
-result = foreach([1,2,3,"hello"], function(val) {
-    return typeof(val);
+result = foreach([1, 2, 3, 'hello'], function (val) {
+    return typeof val;
 });
 
 function foreach(arr, operation) {
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
-        newArr.push(operation(arr[i]));
+        newArr[i] = (operation(arr[i]));
     }
     return newArr;
 }
@@ -51,16 +49,15 @@ function foreach(arr, operation) {
 console.log(result); //this should log ["number", "number", "number", "string"];
 
 // 4.
-result = filter([1,2,3,4,15], function(val) {
-    return val<10; }
-); //this filters each value in the array and only allows values that are less than 10
+result = filter([1, 2, 3, 4, 15], function (val) {
+    return val < 10;
+}); //this filters each value in the array and only allows values that are less than 10
 
-function filter (arr, operation) {
-
+function filter(arr, operation) {
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
         if (operation(arr[i]) == true) {
-            newArr.push(arr[i]);
+            newArr[i] = (arr[i]);
         }
     }
     return newArr;
@@ -69,18 +66,18 @@ function filter (arr, operation) {
 console.log(result); //this should log [1,2,3,4]
 
 // 5.
-result = reject([1,2,3,4,15], function(val) { return val<3; }); //rejects any value that is less than 3
+result = reject([1, 2, 3, 4, 15], function (val) {
+    return val < 3;
+}); //rejects any value that is less than 3
 
 function reject(arr, operation) {
     const newArr = [];
     for (let i = 0; i < arr.length; i++) {
         if (operation(arr[i]) == false) {
-            newArr.push(arr[i]);
+            newArr[i] = arr[i];
         }
     }
     return newArr;
 }
 
 console.log(result); //this should log [3,4,15]
-
-
