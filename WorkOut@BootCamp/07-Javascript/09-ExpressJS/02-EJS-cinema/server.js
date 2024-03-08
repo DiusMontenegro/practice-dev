cac269f85ecbe1448c4a5f16421eccd029490b4f
Dre <montenegrodiussantos1@gmail.com>
2024-03-09 02:01:54 +0800

    JavascriptFUN (Express.js) - EJS Cinema (DONE)

--- WorkOut@BootCamp/07-Javascript/09-ExpressJS/02-EJS-cinema/server.js
+++ WorkOut@BootCamp/07-Javascript/09-ExpressJS/02-EJS-cinema/server.js
@@ -2,13 +2,9 @@
 let express = require('express');
 let app = express();
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/static'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
-app.get("/", function (request, response){
-    response.render('index');
-})
 
 app.get("/movies", function (request, response){
     response.render('movies');
