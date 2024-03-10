b76f1144df834699d2c4d1e649735dc638170a33
Dre <montenegrodiussantos1@gmail.com>
2024-03-10 13:43:48 +0800

    JavascriptFUN (Express.js) - Feedback form (DONE)

---
+++ WorkOut@BootCamp/07-Javascript/09-ExpressJS/05-Feedback-form/server.js
@@ -0,0 +1,30 @@
+// require express
+const express = require('express');
+const session = require('express-session');
+// path module -- try to figure out where and why we use this
+const path = require('path');
+// create the express app
+const app = express();
+const bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded({ extended: true }));
+// static content
+app.use(express.static(path.join(__dirname, './public')));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// root route to render the index.ejs view
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// post route for adding a user
+app.post('/result', function (req, res) {
+    const post = req.body;
+
+    res.render('result', {data: post});
+});
+// tell the express app to listen on port 8000
+app.listen(8000, function () {
+    console.log('listening on port 8000');
+});
