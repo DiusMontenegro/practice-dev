2075b13504247bebcdb0bf5f8aada814b4bfcd6c
Dre <montenegrodiussantos1@gmail.com>
2024-03-11 22:54:15 +0800

    JavascriptFUN (Express.js) - Cryptocurrency setup

--- WorkOut@BootCamp/07-Javascript/09-ExpressJS/TestProject/server.js
+++ WorkOut@BootCamp/07-Javascript/09-ExpressJS/TestProject/server.js
@@ -15,15 +15,39 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 // root route to render the index.ejs view
 app.get('/', function (req, res) {
-    res.render('index');
+    let data =
+    res.render('index', );
 });
 // post route for adding a user
 app.post('/users', function (req, res) {
     console.log('POST DATA', req.body);
     // This is where we would add the user to the database
     // Then redirect to the root route
-    res.redirect('/');
+    res.redirect('/', );
 });
+
+//axios
+const axios = require('axios');
+const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'; // Replace with your actual API URL
+
+app.get('/people', async function(req, res){
+    try {
+        // use the axios .get() method - provide a url and chain the .then() and .catch() methods
+        const response = await axios.get(apiUrl);
+
+        // log the data before moving on!
+        console.log(response.data);
+
+        // rather than rendering, just send back the json data!
+        res.json(response.data);
+    } catch (error) {
+        // log the error before moving on!
+        console.error(error);
+        res.json(error);
+    }
+});
+
+
 // tell the express app to listen on port 8000
 app.listen(8000, function () {
     console.log('listening on port 8000');
