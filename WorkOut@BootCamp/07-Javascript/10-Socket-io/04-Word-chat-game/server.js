796a837cef8ef3d242b3ab2ef0b0577a390e0bec
Dre <montenegrodiussantos1@gmail.com>
2024-03-19 01:11:54 +0800

    JavascriptFUN (Socket.io) - Virtual recitation formatted

--- WorkOut@BootCamp/07-Javascript/10-Socket-io/04-Word-chat-game/server.js
+++ WorkOut@BootCamp/07-Javascript/10-Socket-io/04-Word-chat-game/server.js
@@ -9,24 +9,25 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, './public')));
 
-const users = {}
+const users = {};
 let randomWords = [];
 let randomize = Math.floor(Math.random() * randomWords.length);
-let correctWord = "";
+let correctWord = '';
 
 function newWord() {
     randomize = Math.floor(Math.random() * randomWords.length);
     return randomWords[randomize];
 }
 
-
 app.get('/', async (req, res) => {
     try {
-        const response = await axios.get('https://random-word-api.herokuapp.com/all');
+        const response = await axios.get(
+            'https://random-word-api.herokuapp.com/all'
+        );
         for (let i = 0; i < response.data.length; i++) {
             if (response.data[i].length === 5) {
                 randomWords[randomWords.length] = response.data[i];
-            };
+            }
         }
 
         correctWord = randomWords[randomize];
@@ -39,27 +40,31 @@ app.get('/', async (req, res) => {
 });
 
 io.on('connection', function (socket) {
-    socket.emit("guess", correctWord);
+    socket.emit('guess', correctWord);
 
-    socket.on('new-user', name => {
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
-    })
+    socket.on('new-user', (name) => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    });
 
-    socket.on('send-chat-message', message => {
+    socket.on('send-chat-message', (message) => {
         if (message === correctWord) {
             correctWord = newWord();
-            io.emit("guess", correctWord);
-            io.emit('correct-message', { message: message, name: users[socket.id] });
+            io.emit('guess', correctWord);
+            io.emit('correct-message', {
+                message: message,
+                name: users[socket.id],
+            });
         } else {
-            socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+            socket.broadcast.emit('chat-message', {
+                message: message,
+                name: users[socket.id],
+            });
         }
     });
 
     socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users[socket.id]
-    })
-})
-
-
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
+    });
+});
