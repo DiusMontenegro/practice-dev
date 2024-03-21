514687df724274b4ae57c307e83f346ac7a13dae
Dre <montenegrodiussantos1@gmail.com>
2024-03-21 22:15:47 +0800

    JavascriptFUN (EXAM) - Setup

---
+++ WorkOut@BootCamp/07-Javascript/EXAM/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        password: '[-Mtngro27.Jsmsvr3-]',
+        database: 'express_mvc_cars',
+    },
+    server: {
+        port: process.env.PORT || 8080,
+    },
+};
