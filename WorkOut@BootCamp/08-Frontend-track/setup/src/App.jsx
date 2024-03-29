b9cfe5b4a9c5e5084396aba7880a081cb0506bbc
Dre <montenegrodiussantos1@gmail.com>
2024-03-29 13:14:17 +0800

    React w/ Redux - Clock (DONE)

---
+++ WorkOut@BootCamp/08-Frontend-track/setup/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const useCurrentTime = () => {
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return currentTime;
+};
+
+const App = () => {
+    const currentTime = useCurrentTime();
+
+    return (
+        <div className='mt-[200px] mx-auto flex justify-center items-center h-[300px] w-[300px]'>
+            <h1 className="text-4xl font-mono font-bold text-center text-white bg-gradient-to-r from-purple-500 to-pink-500 rounded-md p-4 shadow-lg">
+                {currentTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', second:'2-digit'})}
+            </h1>
+        </div>
+    );
+};
+
+export default App;
