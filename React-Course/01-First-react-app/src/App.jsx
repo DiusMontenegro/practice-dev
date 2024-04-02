4e94319e2243bbf858e85c6b97522a65468e14c6
Dre <montenegrodiussantos1@gmail.com>
2024-04-03 00:43:01 +0800

    ReactJsFUN - First react app (DONE)

--- React-Course/01-First-react-app/src/App.jsx
+++ React-Course/01-First-react-app/src/App.jsx
@@ -1,17 +1,30 @@
+import { useState, useEffect } from "react";
+import Message from './Message';
+
 const App = () => {
+    const [advice, setAdvice] = useState('');
+    const [count, setCount] = useState(0);
 
     async function getAdvice() {
         const res = await fetch('https://api.adviceslip.com/advice');
         const data = await res.json();
-        console.log(data);
+        if (data) {
+            setAdvice(data.slip.advice);
+            setCount((count) => count + 1);
+        }
     }
+
+    useEffect(() => {
+        getAdvice();
+    },[])
 
     return (
         <div className="flex flex-col justify-center items-center">
-            Advice
+            {advice}
             <button className="border border-black px-4 py-2 rounded-md mt-1" onClick={getAdvice}>
                 Get Advice
             </button>
+            <Message count={count}/>
         </div>
     );
 };
---
+++ React-Course/01-First-react-app/src/Message.jsx
@@ -0,0 +1,9 @@
+
+
+const Message = ({count}) => {
+  return (
+    <h4>You have read {count} piece/s of advice.</h4>
+  )
+}
+
+export default Message
