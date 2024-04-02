690483ac46d1b60cf58e646d663ac559d8f1774c
Dre <montenegrodiussantos1@gmail.com>
2024-04-03 00:46:39 +0800

    ReactJsFUN - First react app (DONE)

--- React-Course/01-First-react-app/src/App.jsx
+++ React-Course/01-First-react-app/src/App.jsx
@@ -20,8 +20,8 @@ const App = () => {
 
     return (
         <div className="flex flex-col justify-center items-center">
-            {advice}
-            <button className="border border-black px-4 py-2 rounded-md mt-1" onClick={getAdvice}>
+            <h1 className="font-bold text-[30px] mb-2 w-[500px] text-center">{advice}</h1>
+            <button className="border border-black px-4 py-2 rounded-md mb-2" onClick={getAdvice}>
                 Get Advice
             </button>
             <Message count={count}/>
