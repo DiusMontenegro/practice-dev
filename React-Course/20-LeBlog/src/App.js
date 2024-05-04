9c515b249e9afb9885ab02709f48063ac3940e01
Dre <montenegrodiussantos1@gmail.com>
2024-05-04 14:43:09 +0800

    ReactJsFUN - Add useMemo to the Context

--- React-Course/20-LeBlog/src/App.js
+++ React-Course/20-LeBlog/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
 import { PostProvider, usePosts } from './PostContext';
 import Test from './Test';
@@ -74,14 +74,14 @@ function Results() {
     return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main() {
+const Main = memo(function Main() {
     return (
         <main>
             <FormAddPost />
             <Posts />
         </main>
     );
-}
+});
 
 function Posts() {
     return (
@@ -135,7 +135,7 @@ function List() {
                     </li>
                 ))}
             </ul>
-            <Test />
+            {/* <Test /> */}
         </>
     );
 }
--- React-Course/20-LeBlog/src/PostContext.js
+++ React-Course/20-LeBlog/src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useMemo, useState } from 'react';
 import { faker } from '@faker-js/faker';
 
 function createRandomPost() {
@@ -34,18 +34,18 @@ function PostProvider({ children }) {
         setPosts([]);
     }
 
+    const value = useMemo(() => {
+        return {
+            posts: searchedPosts,
+            onClearPosts: handleClearPosts,
+            onAddPost: handleAddPost,
+            searchQuery,
+            setSearchQuery,
+        };
+    }, [searchQuery, searchedPosts]);
+
     return (
-        <PostContext.Provider
-            value={{
-                posts: searchedPosts,
-                onClearPosts: handleClearPosts,
-                onAddPost: handleAddPost,
-                searchQuery,
-                setSearchQuery,
-            }}
-        >
-            {children}
-        </PostContext.Provider>
+        <PostContext.Provider value={value}>{children}</PostContext.Provider>
     );
 }
 
