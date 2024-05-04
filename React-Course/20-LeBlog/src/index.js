da06e9c47f3fd6c835a11d9da4efb745fad8287a
Dre <montenegrodiussantos1@gmail.com>
2024-05-04 11:00:39 +0800

    ReactJsFUN - LeBlog useMemo & useCallback

--- React-Course/20-LeBlog/src/index.js
+++ React-Course/20-LeBlog/src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./style.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import App from './App-memo';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
