683260da4520b8243637631a1683f2ed48f0dc45
Dre <montenegrodiussantos1@gmail.com>
2024-05-21 21:52:02 +0800

    DuxBank - create and test the store

--- React-Course/22-DuxBank/src/store.js
+++ React-Course/22-DuxBank/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const initialState = {
     balance: 0,
     loan: 0,
@@ -12,7 +14,12 @@ function reducer(state = initialState, action) {
             return { ...state, balance: state.balance - action.payload };
         case 'account/requestLoan':
             if (state.loan > 0) return state;
-            return { ...state, loan: action.payload };
+            return {
+                ...state,
+                balance: state.balance + action.payload.amount,
+                loan: action.payload.amount,
+                loanPurpose: action.payload.purpose,
+            };
         case 'account/payLoan':
             return {
                 ...state,
@@ -24,3 +31,16 @@ function reducer(state = initialState, action) {
             return state;
     }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: 'account/deposit', payload: 500 });
+store.dispatch({ type: 'account/withdraw', payload: 200 });
+store.dispatch({
+    type: 'account/requestLoan',
+    payload: { amount: 1000, purpose: 'Buy a Laptop' },
+});
+store.dispatch({
+    type: 'account/payLoan',
+});
+console.log(store.getState());
