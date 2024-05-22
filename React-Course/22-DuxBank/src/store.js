786d20bdb2a7bedbe38b37379d17539156a06f48
Dre <montenegrodiussantos1@gmail.com>
2024-05-22 13:19:24 +0800

    DuxBank - add action creator functions

--- React-Course/22-DuxBank/src/store.js
+++ React-Course/22-DuxBank/src/store.js
@@ -34,13 +34,36 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: 'account/deposit', payload: 500 });
-store.dispatch({ type: 'account/withdraw', payload: 200 });
-store.dispatch({
-    type: 'account/requestLoan',
-    payload: { amount: 1000, purpose: 'Buy a Laptop' },
-});
-store.dispatch({
-    type: 'account/payLoan',
-});
+// store.dispatch({ type: 'account/deposit', payload: 500 });
+// store.dispatch({ type: 'account/withdraw', payload: 200 });
+// store.dispatch({
+//     type: 'account/requestLoan',
+//     payload: { amount: 1000, purpose: 'Buy a Laptop' },
+// });
+// store.dispatch({
+//     type: 'account/payLoan',
+// });
+
+// Action Creator function
+function deposit(amount) {
+    return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+    return { type: 'account/withdraw', payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+    return { type: 'account/requestLoan', payload: { amount, purpose } };
+}
+
+function payLoan() {
+    return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, 'Buy a Car'));
+store.dispatch(payLoan());
+
 console.log(store.getState());
