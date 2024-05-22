680d3cbee3b3f5fb33d6352d22e5418f1e4d625c
Dre <montenegrodiussantos1@gmail.com>
2024-05-22 22:50:21 +0800

    DuxBank - add react-redux

--- React-Course/22-DuxBank/src/main.jsx
+++ React-Course/22-DuxBank/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
 import './index.css';
 import store from './store.js';
 
-store.dispatch({ type: 'account/deposit', payload: 300 });
-console.log(store.getState());
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
