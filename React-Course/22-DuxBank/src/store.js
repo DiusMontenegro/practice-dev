88758a02dfed3cd146d82f0ceeacc28952da9201
Dre <montenegrodiussantos1@gmail.com>
2024-05-22 13:49:49 +0800

    DuxBank - add customer reducer

--- React-Course/22-DuxBank/src/store.js
+++ React-Course/22-DuxBank/src/store.js
@@ -1,12 +1,18 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
-const initialState = {
+const initialStateAccount = {
     balance: 0,
     loan: 0,
     loanPurpose: '',
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+    fullName: '',
+    nationalID: '',
+    createdAt: '',
+};
+
+function accountReducer(state = initialStateAccount, action) {
     switch (action.type) {
         case 'account/deposit':
             return { ...state, balance: state.balance + action.payload };
@@ -32,7 +38,28 @@ function reducer(state = initialState, action) {
     }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+    switch (action.type) {
+        case 'customer/createCustomer':
+            return {
+                ...state,
+                fullName: action.payload.fullName,
+                nationalID: action.payload.nationalID,
+                createdAt: action.payload.createdAt,
+            };
+        case 'customer/updateName':
+            return { ...state, fullName: action.payload };
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    account: accountReducer,
+    customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
 
 // store.dispatch({ type: 'account/deposit', payload: 500 });
 // store.dispatch({ type: 'account/withdraw', payload: 200 });
@@ -66,4 +93,22 @@ store.dispatch(withdraw(200));
 store.dispatch(requestLoan(1000, 'Buy a Car'));
 store.dispatch(payLoan());
 
+function createCustomer(fullName, nationalID) {
+    return {
+        type: 'customer/createCustomer',
+        payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+        },
+    };
+}
+
+function updateName(fullName) {
+    return { type: 'customer/updateName', payload: fullName };
+}
+
+store.dispatch(createCustomer('Dius Montenegro', '1234'));
+console.log(store.getState());
+store.dispatch(updateName('Dre Montenegro'));
 console.log(store.getState());
