c6e862abef5844520edc6163322d4d3b3410e7c3
Dre <montenegrodiussantos1@gmail.com>
2024-05-25 23:33:49 +0800

    DuxBank - Add redux toolkit and configure store

--- React-Course/22-DuxBank/src/features/accounts/AccountOperations.jsx
+++ React-Course/22-DuxBank/src/features/accounts/AccountOperations.jsx
@@ -22,6 +22,7 @@ function AccountOperations() {
 
         dispatch(deposit(depositAmount, currency));
         setDepositAmount('');
+        setCurrency('USD');
     }
 
     function handleWithdrawal() {
---
+++ React-Course/22-DuxBank/src/store-v2.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { thunk } from 'redux-thunk';
+import accountReducer from './features/accounts/accountSlice';
+import customerReducer from './features/customers/customerSlice';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const rootReducer = combineReducers({
+    account: accountReducer,
+    customer: customerReducer,
+});
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
