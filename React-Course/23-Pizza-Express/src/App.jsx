022e707e16b936834768fd810b31ee05d236a87c
Dre <montenegrodiussantos1@gmail.com>
2024-05-26 16:56:48 +0800

    Pizza Express - Initial Upload

---
+++ React-Course/23-Pizza-Express/src/App.jsx
@@ -0,0 +1,36 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
+import CreateOrder from './features/order/CreateOrder';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/menu',
+        element: <Menu />,
+    },
+    {
+        path: '/cart',
+        element: <Cart />,
+    },
+    {
+        path: '/order/:orderId',
+        element: <Order />,
+    },
+    {
+        path: '/order/new',
+        element: <CreateOrder />,
+    },
+]);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
