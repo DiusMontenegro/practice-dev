7be0e03381e53caa19c9c75c718d53d587de745d
Dre <montenegrodiussantos1@gmail.com>
2024-05-27 22:29:24 +0800

    Pizza Express - Handle form validation, errors, requests and actions

--- React-Course/23-Pizza-Express/src/App.jsx
+++ React-Course/23-Pizza-Express/src/App.jsx
@@ -6,7 +6,9 @@ import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import Order, { loader as orderLoader } from './features/order/Order';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, {
+    action as createOrderAction,
+} from './features/order/CreateOrder';
 
 const router = createBrowserRouter([
     {
@@ -36,6 +38,7 @@ const router = createBrowserRouter([
             {
                 path: '/order/new',
                 element: <CreateOrder />,
+                action: createOrderAction,
             },
         ],
     },
