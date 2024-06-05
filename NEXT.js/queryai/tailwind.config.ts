d0a5c7254d263eb2f2cdd30877d4c04254764096
Dre <montenegrodiussantos1@gmail.com>
2024-06-05 11:36:24 +0800

    Implement Initial tailwindcss styling

--- NEXT.js/queryai/tailwind.config.ts
+++ NEXT.js/queryai/tailwind.config.ts
@@ -1,23 +1,3 @@
-<<<<<<< Updated upstream
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
-        },
-    },
-    plugins: [],
-=======
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
@@ -97,5 +77,4 @@ module.exports = {
         require("tailwindcss-animate"),
         require("@tailwindcss/typography"),
     ],
->>>>>>> Stashed changes
 };
